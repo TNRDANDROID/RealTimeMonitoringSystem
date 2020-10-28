6ef1d3a01d963e97e0be654c872256dadcec81d7
Preethika <tnrdmobile@gmail.com>
2020-10-28 14:57:07 +0530

    multiple village worklist in local db

--- app/src/main/java/com/nic/RealTimeMonitoringSystem/activity/WorkListScreen.java
+++ app/src/main/java/com/nic/RealTimeMonitoringSystem/activity/WorkListScreen.java
@@ -356,9 +356,9 @@ public class WorkListScreen extends AppCompatActivity implements View.OnClickLis
 
             dbData.open();
             /*In online Delete DB to fetch the Api data*/
-            if(Utils.isOnline()){
-                dbData.deleteWorkListTable();
-            }
+//            if(Utils.isOnline()){
+//                dbData.deleteWorkListTable();
+//            }
             ArrayList<RealTimeMonitoringSystem> workList_count = dbData.getAllWorkLIst("insert",pref_finYear,prefManager.getDistrictCode(),prefManager.getBlockCode(),prefManager.getPvCode(),0);
            if (workList_count.size() <= 0) {
                 if (params.length > 0) {
